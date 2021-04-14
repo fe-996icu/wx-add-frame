d15c90ec07cef24e5e1582f92ab10da9f9e2fe31
zhangjianzhong <zhangjianzhong@yiside.com>
2021-04-14 15:16:05 +0800

    提交

--- pages/cut/index.js
+++ pages/cut/index.js
@@ -1,6 +1,7 @@
 import regeneratorRuntime from 'regenerator-runtime'
 import computedBehavior from 'miniprogram-computed'
 import sys from '../../utils/sys'
+import * as utils from '../../utils/index'
 
 const app = getApp()
 
@@ -10,55 +11,119 @@ Page({
 	canvas: null,
 
 	data: {
-		img: '../../assets/imgs/img.png',
-		imgSize: {
-			w: 0,
-			h: 0,
+		// 操作图片路径
+		img: null,
+		// 图片原始尺寸
+		imageOriginSize: null,
+		// imageOriginSize: {
+		// 	width: null,
+		// 	height: null,
+		// },
+		// 图片展示尺寸
+		imageViewSize: null,
+		// imageViewSize: {
+		// 	width: null,
+		// 	height: null,
+		// },
+		// 拖拽区域矩形信息
+		handleBoxRect: {
+			width: null,
+			height: null,
+			top: 0,
+			left: 0,
 		},
 
 		maxSize: {
 			w: 0,
 			h: 0,
 		},
-		aa: 'abc'
 	},
 
 	computed: {
 		editorBoxStyle(data){
-			const { w, h } = data.imgSize;
+			if(data.imageViewSize){
+				const { width, height, } = data.imageViewSize;
 
-			return `
-				width: ${w}px;
-				height: ${h}px;
-			`;
+				return `
+					width: ${width}px;
+					height: ${height}px;
+				`;
+			}
+		},
+		handleBoxStyle(data){
+			if(data.imageViewSize){
+				const { width, height, top, left, } = data.handleBoxRect;
+
+				return `
+					width: ${width}px;
+					height: ${height}px;
+					top: ${top}px;
+					left: ${left}px;
+				`;
+			}
 		},
 	},
 
 	onLoad(){
-		this.init();
+		// this.computedMaxEditorBoxArea();
+
+		this.init('../../assets/imgs/img.jpg');
 	},
 
-	async init(){
-		const { img } = this.data;
+	// 计算编辑区支持的最大容器尺寸
+	async computedMaxEditorBoxArea(){
+		return new Promise((resolve)=>{
+			const query = wx.createSelectorQuery();
+			query.select('.editor-box').boundingClientRect((rect)=>{
+				resolve(rect);
+			}).exec();
+		})
+	},
+
+	async init(img){
+		const rect = await this.computedMaxEditorBoxArea();
+		this.setData({
+			maxSize: {
+				w: rect.width,
+				h: rect.height,
+			},
+		})
+		console.log('编辑区editor-box最大编辑尺寸：', rect);
+
+		// const { img } = this.data;
 		const res = await sys.getImageInfo({
 			src: img,
 		});
 
 		this.setData({
-			imgSize: {
-				w: res.width,
-				h: res.height,
+			img,
+			imageOriginSize: {
+				width: res.width,
+				height: res.height,
 			},
 		});
 
-		const query = wx.createSelectorQuery();
-		query.select('.operate-area').boundingClientRect((rect)=>{
-			this.setData({
-				maxSize: {
-					w: rect.width,
-					h: rect.height,
-				},
-			});
-		}).exec();
+		// 计算完全展示的图片尺寸
+		const size = utils.getCompressSize({
+			limitWidth: rect.width,
+			limitHeight: rect.height,
+			width: res.width,
+			height: res.height,
+		});
+
+		this.setData({
+			imageViewSize: {
+				width: size.width,
+				height: size.height,
+			},
+			handleBoxRect: {
+				width: size.width,
+				height: size.height,
+				top: 0,
+				left: 0,
+			},
+		});
+
+		console.log(size, 'size');
 	}
 })
--- utils/index.js
+++ utils/index.js
@@ -1,27 +1,23 @@
 
 const getCompressSize = (function(){
-	/**图片最大不能超过500kb */
-	const LIMIT_SIZE = 1024 * 500;
 	/**图片最大宽度不能超过1080px */
-	const LIMIT_WIDTH = 1080;
+	const LIMIT_WIDTH = 335;
 	/**图片最大高度不能超过1920px */
-	const LIMIT_HEIGHT = 1920;
+	const LIMIT_HEIGHT = 483;
 
-	return function({ size, width, height, type, }){
-		let isCompressd = false;
+	return function({ width, height, limitWidth=LIMIT_WIDTH, limitHeight=LIMIT_HEIGHT }){
 		let compressWidth = width;
 		let compressHeight = height;
 
 		// 宽度是否超出限制
-		if(width > LIMIT_WIDTH){
-			const aspectRatio = width / LIMIT_WIDTH;
-			compressWidth = LIMIT_WIDTH;
+		if(width > limitWidth){
+			const aspectRatio = width / limitWidth;
+			compressWidth = limitWidth;
 			compressHeight = Math.round(height / aspectRatio);
 
 			// 如果等比例缩放后，高度还是超出限制，继续计算
-			if(compressHeight > LIMIT_HEIGHT){
-				const finalSize = _getCompressSize({
-					size,
+			if(compressHeight > limitHeight){
+				const finalSize = getCompressSize({
 					width: compressWidth,
 					height: compressHeight,
 				});
@@ -29,18 +25,15 @@ const getCompressSize = (function(){
 				compressWidth = finalSize.width;
 				compressHeight = finalSize.height;
 			}
-
-			isCompressd = true;
-		}else if(height > LIMIT_HEIGHT){		// 高度是否超出限制
+		}else if(height > limitHeight){		// 高度是否超出限制
 			// 按照实际高度和最大高度比例，等比例缩放宽度
-			const aspectRatio = height / LIMIT_HEIGHT;
+			const aspectRatio = height / limitHeight;
 			compressWidth = Math.round(width / aspectRatio);
-			compressHeight = LIMIT_HEIGHT;
+			compressHeight = limitHeight;
 
 			// 如果等比例缩放后，宽度还是超出限制，继续计算
-			if(compressWidth > LIMIT_WIDTH){
-				  const finalSize = _getCompressSize({
-					size,
+			if(compressWidth > limitWidth){
+				  const finalSize = getCompressSize({
 					width: compressWidth,
 					height: compressHeight,
 				});
@@ -48,26 +41,23 @@ const getCompressSize = (function(){
 				compressWidth = finalSize.width;
 				compressHeight = finalSize.height;
 			}
-
-			isCompressd = true;
-		}else if(size > LIMIT_SIZE || type == 'png'){
-			// 宽度和高度都没超出限制，但文件大小超出范围
-			isCompressd = true;
 		}
 
 		return {
-			isCompressd,
 			width: compressWidth,
 			height: compressHeight,
 		}
 	}
 })();
 
+// const ret = getCompressSize({
+// 	width: 750,
+// 	height: 1334,
+// });
 
-const ret = getCompressSize({
-	size: 100,
-	width: 1920,
-	height: 1080,
-});
+// console.log(ret, 'ret');
 
-console.log(ret, 'ret');
+
+export {
+	getCompressSize,
+}
